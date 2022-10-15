53a571cce8ab9a1f9adef3b7c9cfcec4dcefafee
Aiman <ansariaiman2000@gmail.com>
2022-10-15 07:00:49 -0700

    fix: Connected to MongoDB added connection path manually

--- server.js
+++ server.js
@@ -1,42 +1,39 @@
-const express = require('express');
-const connectDB = require('./utils/db');
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('./docs/swagger.json');
+const express = require("express");
+const connectDB = require("./utils/db");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./docs/swagger.json");
 
 //load env vars
-require('dotenv').config()
+require("dotenv").config();
 
 //Connect to database
 connectDB();
 
 //route files
-const userRoutes = require('./routes/user.routes');
-const scheduleRoutes = require('./routes/schedule.routes');
-const roomRoutes = require('./routes/room.routes');
+const userRoutes = require("./routes/user.routes");
+const scheduleRoutes = require("./routes/schedule.routes");
+const roomRoutes = require("./routes/room.routes");
 const app = express();
 
 //body parser
 app.use(express.json());
 
 // Swagger Documentation
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 //mount routers
-app.use('/api/v1/users', userRoutes);
-app.use('/api/v1/room', roomRoutes);
-app.use('/api/v1/schedule', scheduleRoutes);
+app.use("/api/v1/users", userRoutes);
+app.use("/api/v1/room", roomRoutes);
+app.use("/api/v1/schedule", scheduleRoutes);
 
-
-const PORT = process.env.PORT;
-
-const server = app.listen(PORT, function () {
-    console.log(`Server running on ${process.env.NODE_ENV} mode on port ${PORT}`)
+const server = app.listen(3000, function () {
+  console.log(`Server running on port 3000`);
 });
 
 //handle unhandled PromiseRejection
-process.on('unhandledRejection', (err) => {
-    console.log(`Error: ${err.message}`.red);
+process.on("unhandledRejection", (err) => {
+  console.log(`Error: ${err.message}`.red);
 
-    //Close Server & exit process
-    server.close(() => process.exit(1));
-})
+  //Close Server & exit process
+  server.close(() => process.exit(1));
+});
--- utils/db.js
+++ utils/db.js
@@ -1,21 +1,15 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-    try {
-        const mongo_host = process.env.MONGOHOST;
-        const mongo_port = process.env.MONGOPORT;
-        const database_name = process.env.DATABASENAME;
-
-        let mongoString =
-            "mongodb://" + mongo_host + ":" + mongo_port + "/" + database_name;
-        await mongoose.connect(mongoString, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-        console.log(`MongoDB Connected: ${mongoString}`);
-    } catch (err) {
-        console.error(err, "Error");
-    }
+  try {
+    await mongoose.connect("mongodb://localhost:27017/User", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`MongoDB Connected`);
+  } catch (err) {
+    console.error(err, "Not connected");
+  }
 };
 
 module.exports = connectDB;
