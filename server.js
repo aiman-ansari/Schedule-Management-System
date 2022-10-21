d60e4dcb430ef501a906893b2d17daaf272fabc4
Aiman <ansariaiman2000@gmail.com>
2022-10-21 03:01:37 -0700

    heroku deployment

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const connectDB = require("./utils/db");
 const cors = require("cors");
+const path = require("path");
 const swaggerUi = require("swagger-ui-express");
 const swaggerDocument = require("./docs/swagger.json");
 //load env vars
@@ -13,7 +14,7 @@ const userRoutes = require("./routes/user.routes");
 const scheduleRoutes = require("./routes/schedule.routes");
 const roomRoutes = require("./routes/room.routes");
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 app.use(
   cors({
     origin: "*",
@@ -21,11 +22,7 @@ app.use(
 );
 //body parser
 app.use(express.json());
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   next();
-// });
-// Swagger Documentation
+
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 //mount routers
@@ -33,6 +30,14 @@ app.use("/api/v1/users", userRoutes);
 app.use("/api/v1/room", roomRoutes);
 app.use("/api/v1/schedule", scheduleRoutes);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("schedule-management-react/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(
+      path.join(__dirname, "schedule-management-react", "build", "index.html")
+    );
+  });
+}
 const server = app.listen(port, function () {
   console.log(`Server running on port ${port}`);
 });
--- utils/db.js
+++ utils/db.js
@@ -2,11 +2,14 @@ const mongoose = require("mongoose");
 // const { UUID } = require("bson");
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/ScheduleDatabase", {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      //   pkFactory: { createPk: () => new UUID().toBinary() },
-    });
+    await mongoose.connect(
+      process.env.MONGODB_URI || "mongodb://localhost:27017/ScheduleDatabase",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        //   pkFactory: { createPk: () => new UUID().toBinary() },
+      }
+    );
     console.log(`MongoDB Connected`);
   } catch (err) {
     console.error(err, "Not connected");
